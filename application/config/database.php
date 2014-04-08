d62ed7b0992867a1ed58d7bd7a7380c442c4b42b
Pierre Dechery <pierre@decheryweb.com.br>
2014-04-08 06:56:38 -0300

    Configuração db

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'quemprecisa';
+$db['default']['password'] = 'lalala';
+$db['default']['database'] = 'doacoes';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
