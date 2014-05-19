e2f815d1ddfdc39da42d209bcfbf9f4a0dcac439
Pierre Dechery <pierre@decheryweb.com.br>
2014-05-18 21:15:31 -0300

    Iniciando HTML de Interesse

--- application/views/interesse_single.php
+++ application/views/interesse_single.php
@@ -8,32 +8,28 @@
 
 	$user_id = $login_data['user_id'];
 ?>
-<table width="95%" style="color: <?php echo $cor?>;" class="interesse_row">
+<table style="color: <?php echo $cor?>;" class="interesse_row">
 	<tr>
-	<td width="25%">
-	<?php echo $interesse->categoria?>
-	</td>
-	<td width="25%">
-		<select id="raio_<?php echo $interesse->id?>" style="width: 100px;">
-<?php
-	$raios = $params['raios_busca'];
-	$selected = "";
-	foreach($raios as $raio => $desc) {
-		$selected=($raio==$interesse->raio_busca)?"selected":"";
-		echo '<option value="'.$raio.'" '.$selected.'>'.$desc.'</option>';
-	}
-?>
-		</select>
-	</td>
-	<td width="10%">
-	&nbsp;
-	</td>
-	<td width="15%">
-	<?php echo $interesse->data?>
-	</td>
-	<td width="25%">
-	<input type="button" value="Atualizar" class="update_interesse_btn" data-catid="<?php echo $interesse->id?>">		
-	<input type="button" class="activ_interesse_btn" value="<?php echo $labelBtn?>" data-catid="<?php echo $interesse->id?>"/>
-	</td>
+		<td><?php echo $interesse->categoria?></td>
+		<td>
+			<select id="raio_<?php echo $interesse->id?>" style="width: 100px;">
+				<?php
+				$raios = $params['raios_busca'];
+				$selected = "";
+				foreach($raios as $raio => $desc) {
+					$selected=($raio==$interesse->raio_busca)?"selected":"";
+					echo '<option value="'.$raio.'" '.$selected.'>'.$desc.'</option>';
+				}
+				?>
+			</select>
+		</td>
+		<td></td>
+		<td>
+			<?php echo $interesse->data?>
+		</td>
+		<td>
+			<input type="button" value="Atualizar" class="update_interesse_btn" data-catid="<?php echo $interesse->id?>">		
+			<input type="button" class="activ_interesse_btn" value="<?php echo $labelBtn?>" data-catid="<?php echo $interesse->id?>"/>
+		</td>
 	</tr>
 </table>
