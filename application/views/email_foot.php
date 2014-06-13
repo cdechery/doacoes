f529e16ada9ff0a554d10939891db96ec9897ae9
Christian Dechery <cdechery@gmail.com>
2014-06-13 13:49:35 -0300

    Revisão e Fix nos Emails

--- application/views/email_foot.php
+++ application/views/email_foot.php
@@ -1,1 +1,2 @@
-<p>Se nÃ£o quiser mais receber emails, clique aqui.</p>
+<p>Atenção: o site QuemPrecisa apenas inicia o contato entre pessoas. Toda comunicação posterior é realizada diretamente via email. Ao responder este email, você estará revelando seu endereço de email para a outra parte.</p>
+<p>Se não quiser mais receber emails, clique aqui.</p>
