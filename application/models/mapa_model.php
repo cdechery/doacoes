fa98e9d83f6e4b45f1ed0941804a4c846b626a0f
Christian Dechery <cdechery@gmail.com>
2014-07-15 09:46:37 -0300

    Query para montar o mapa

--- application/models/mapa_model.php
+++ application/models/mapa_model.php
@@ -16,19 +16,26 @@ class Mapa_model extends MY_Model {
 		$this->db->join('item i', 'u.id = i.usuario_id');
 		$this->db->join('categoria c', 'c.id = i.categoria_id');
 		$this->db->join('situacao s', 's.id = i.situacao_id');
+		$this->db->where('u.tipo','P'); //Pessoa
 		$this->db->where('i.status','I'); //Disponivel
 		$this->db->order_by('u.id', 'asc');
 		$pessoas = $this->db->get()->result();
 
-		return $pessoas;
 
-		// $this->db->select('u.id as user_id, u.lat, u.lng, 
-		// 	u.tipo, ifnull(c.id,0) as cat_id, 0 as sit_id', FALSE);
-		// $this->db->from('usuario u');
-		// $this->db->join('categoria c', 'c.id = i.categoria_id', 'left');
-		// $this->db->order_by('u.id', 'asc');
-		// $insts = $this->db->get()->result();
+		$this->db->select('u.id as user_id, u.lat, u.lng, 
+			u.tipo, ifnull(c.id,0) as cat_id, ifnull(s.id,0) as sit_id,
+			ifnull(it.categoria_id,0) as int_id', FALSE);
+		$this->db->from('usuario u');
+		$this->db->join('interesse it',
+			'u.id = it.usuario_id AND it.fg_ativo = \'S\'', 'left');
+		$this->db->join('item i', 'u.id = i.usuario_id', 'left');
+		$this->db->join('categoria c', 'c.id = i.categoria_id');
+		$this->db->join('situacao s', 's.id = i.situacao_id');
+		$this->db->where('u.tipo','I'); //Instituicao
+		$this->db->where('i.status','I'); //Disponivel
+		$this->db->order_by('u.id', 'asc');
+		$insts = $this->db->get()->result();
 
-		// return array_merge($pessoas, $insts);
+		return array_merge($pessoas, $insts);
 	}
 }
