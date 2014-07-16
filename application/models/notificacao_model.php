8fcf635add9eee6940cd37ef5796b1976659c1df
Christian Dechery <cdechery@gmail.com>
2014-07-16 14:27:03 -0300

    Fix insert_item na Notificacao

--- application/models/notificacao_model.php
+++ application/models/notificacao_model.php
@@ -8,7 +8,8 @@ class Notificacao_model extends MY_Model {
 
 	public function insert_item( $item_id ) {
 		$insert_data = array(
-			'item_id' => $item_id
+			'item_id' => $item_id,
+			'usuario_id' => -1
 		);
 
 		return $this->db->insert('controle_notif_email', $insert_data);
