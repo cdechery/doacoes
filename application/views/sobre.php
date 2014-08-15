1ef4e502b8130dadcb623358cb6d89732b6db8c6
Christian Dechery <cdechery@gmail.com>
2014-08-15 17:00:19 -0300

    Sobre e Contato (apenas texto)

--- application/views/sobre.php
+++ application/views/sobre.php
@@ -7,17 +7,25 @@
 
 	<div class="col">
 		<h4>para pessoas:</h4>
-		<p>Se você estiver procurando alguém interessado em algo que você não precisa ou não usa mais, basta você se cadastrar, depois incluir os itens que tem aí sobrando e acumulando pó. No mapa você consegue filtrar as pessoas interessadas nas categorias de item que você cadastrou. Se elas tiverem 'Interesses' cadastrados, serão avisadas automaticamente das coisas que você cadastrar.
+		<p>Se você estiver procurando alguém interessado em algo que você não precisa ou não usa mais, basta você se cadastrar, depois incluir os itens que tem aí sobrando e acumulando pó. No mapa você consegue filtrar as pessoas interessadas nas categorias de item que você cadastrou. Se elas tiverem <i>Interesses</i> cadastrados, serão avisadas automaticamente das coisas que você cadastrar.
 	</div>
 
 	<div class="col">
 		<h4>para instituições:</h4>
-		<p>Usuários cadastrados como Instituições podem cadastrar Itens e Interesses da mesma forma que Pessoas. A diferença é que a validade dos Interesses é maior, e, como o foco da Instiuição é mais em receber itens do que doar, todas são exibidas no mapa independente de ter algo a doar ou não - dessa forma fica mais fácil achar alguma Interessada em receber aquilo que você quer doar.	
+		<p>Usuários cadastrados como <i>Instituições</i> podem cadastrar Itens e Interesses da mesma forma que Pessoas. A diferença é que a validade dos Interesses é maior, e, como o foco da Instiuição é mais em receber itens do que doar, todas são exibidas no mapa independente de ter algo a doar ou não - dessa forma fica mais fácil achar alguma Interessada em receber aquilo que você quer doar.	
 	</div>
 
-	<br clear="left">
+	<br clear="all">
 
 	<h3>Contato para doações</h3>
 	<p>O site atua apenas como um grande cadastro de pessoas, instituições e itens disponíveis para doação. Todo contato e negociação para realização das coletas e ou entrega dos itens, é feito diretamente entre as pessoas - e o Interessa.org facilita (e protege) esse primeiro contato. Ao identificar um item que lhe interessa, por exemplo, você envia uma mensagem ao proprietário, se ele decidir responder, a partir desse momento toda a comunicação é feita diretamente por email.</p>
 
+	<h3>Trocas e vendas</h3>
+	<p>Trocas e vendas são proibidas aqui. O foco aqui é o desapego. Por favor, colabore.</p>
+
+	<h3>Sustentabilidade</h3>
+	<p>Além do nosso site estimular o segundo 'R' que forma os alicerces da sustentabilidade (Reduzir, Reutilizar, Reciclar), tentamos ir além, sempre que possível. Esse site está hospedado com uma empresa que tem 300% da energia que utiliza em fontes renováveis. Através de turbinas eólicas, além de serem totalmente independentes da rede elétrica, ainda sobra, e eles vendem de volta - garantindo emissão negativa de carbono.</p>
+
+	<h3>Alguma dúvida?</h3>
+	<p>Para maiores detalhes sobre as regras de funcionamento, recomendamos que leia nossos <a href="<?php echo static_url('termos')?>">Termos de Serviço</a>. Se mesmo assim ainda restar alguma dúvida ou se quiser entrar em contato conosco por outro motivo, <a href='<?php echo static_url('contato')?>'>nos escreva</a>. Teremos prazer em responder.</p>
 </div>
