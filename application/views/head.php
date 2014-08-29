b6aa84e1751e3b54a970ad7a29ee5f4d407b2db1
Christian Dechery <cdechery@gmail.com>
2014-08-29 11:11:02 -0300

    Google Analytics

--- application/views/head.php
+++ application/views/head.php
@@ -27,6 +27,18 @@
 	}
 
 ?>
+<?php if( ENVIRONMENT=='production'): ?>
+<script async>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54303875-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php endif; ?>
 <script type="application/javascript" src="<?php echo static_url('javascript')?>"></script>
 <script type="application/javascript" src="<?php echo static_url('min/g='.$min_template.'_js'.$min_debug)?>"></script>
 <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
