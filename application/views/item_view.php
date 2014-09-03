aab055f4e0b91c5cd4d6a677f2f842e4b65f58ea
Christian Dechery <cdechery@gmail.com>
2014-09-03 16:21:16 -0300

    Melhorias item_view e hover no #user-menu

--- application/views/item_view.php
+++ application/views/item_view.php
@@ -1,19 +1,11 @@
-<?php
-	$temImagens = false;
-	$height = "";
-	if( isset($imgdata) && count($imgdata)>0 ) {
-		$temImagens = true;
-		$height = "style='height: 340px;'";
-	}
-?>
 <header>
 	<h3><?php echo $idata['titulo']?></h3>
 </header>
-<div id="main" <?php echo $height?> >
+<div id="main">
 	<p><?php echo nl2br(wordwrap($idata['descricao'],60))?></p>
 	<div class="imgs">
 	<?php
-		if( $temImagens ) {
+		if( isset($imgdata) && count($imgdata)>0 ) {
 			foreach ($imgdata as $img) {
 	?>
 		<a href="<?php echo user_img_url($img->nome_arquivo)?>" class="fancybox" rel="<?php echo $idata['id']?>" title="<?php echo $idata['titulo']?>"><img src="<?php echo item_image($img->nome_arquivo, 120)?>"></a>
@@ -22,9 +14,9 @@
 		}
 	?>
 	</div>
-	<?php
-		if( $login_data['logged_in'] ) {
-			echo meinteressa_button( $idata['id'] );
-		}
-	?>
+	<?php if( $login_data['logged_in'] ): ?>
+	<div style="height: 30px;">
+		<?php echo meinteressa_button( $idata['id'] ); ?>
+	</div>
+	<?php endif; ?>
 </div>
