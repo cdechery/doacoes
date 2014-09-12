b6390a3ef1cd3538fddb99164f66fb3fcf6a7ccd
Christian Dechery <cdechery@gmail.com>
2014-09-12 09:41:23 -0300

    FIX Botão do facebook - Pagina login

--- application/views/login.php
+++ application/views/login.php
@@ -56,6 +56,6 @@ $(document).ready(function() {
 </div>
 
 <div id="link_cadastro">
-	Novo por aqui?<br><a href="<?php echo base_url('usuario/escolhe_tipo/w')?>" class="signup link escolhetipo_box fancybox.ajax">Preencha o cadastro</a> ou <a  class="signup link" href="#" onClick="fb_login();">Conecte-se <i class="fa fa-facebook-square"></i></a>
+	Novo por aqui?<br><a href="<?php echo base_url('usuario/escolhe_tipo/w')?>" class="signup link escolhetipo_box fancybox.ajax">Preencha o cadastro</a> ou Conecte-se pelo <a style="vertical-align: bottom" class="fb-login-button" scope="email,public_profile" data-size="icon" data-show-faces="false"></a>
 </div>
 
